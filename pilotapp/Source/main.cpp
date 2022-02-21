a5e8c65763c1acb68882d7ade4b72ac44238678b
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2022-02-20 19:43:42 -0800

    Add style sheet

--- pilotapp/Source/main.cpp
+++ pilotapp/Source/main.cpp
@@ -1,9 +1,25 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QFile>
+
+void setupStyleSheet(QApplication* a){
+    QFile sheet(":/style.qss");
+
+    if (sheet.open(QIODevice::ReadOnly | QIODevice::Text)){
+        a->setStyleSheet(sheet.readAll());
+        sheet.close();
+    }
+    
+}
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+
+    setupStyleSheet(&a);
+
+    //
+
     MainWindow w;
 
     w.setFixedSize(800, 480); // official raspberry pi display size
