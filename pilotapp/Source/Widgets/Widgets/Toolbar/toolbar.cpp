ed7459a309cb64c6433cb6b0852046d073d264d1
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2022-02-24 21:27:52 -0800

    Upload icons + create icon buttons

--- pilotapp/Source/Widgets/Widgets/Toolbar/toolbar.cpp
+++ pilotapp/Source/Widgets/Widgets/Toolbar/toolbar.cpp
@@ -2,6 +2,8 @@
 
 #include "../../../Utilities/utilities.h"
 
+#include <QDebug>
+
 toolbarWidget::toolbarWidget(QWidget* parent) {
 
 	this->setMaximumHeight(parent->height() * heightRatio);
@@ -10,8 +12,42 @@ toolbarWidget::toolbarWidget(QWidget* parent) {
 
 	utilities::setBackgroundColor(this, Qt::black);
 
+	//
+
+	hLayout = new QHBoxLayout(this);
+	hLayout->setContentsMargins(0,0,0,0);
+
+	setupButtons();
+
 }
 
 toolbarWidget::~toolbarWidget() {
 
+}
+
+//
+
+void toolbarWidget::setupButtons(){
+	for (int i = 0; i < toolbarButtonSize; i++){
+		toolbarButtons[i] = new QPushButton(this);
+		toolbarButtons[i]->setObjectName("iconPushButton");
+		
+		toolbarButtons[i]->setMaximumWidth(this->height());
+		toolbarButtons[i]->setFixedSize(this->height(), this->height());
+
+		hLayout->addWidget(toolbarButtons[i]);
+
+		//
+
+		QPixmap pmap(("/Assets/Icons/" + toolBarButtonIconNames[i]));
+		QIcon ic(pmap);
+
+		qInfo() << pmap.isNull();
+
+		toolbarButtons[i]->setIcon(ic);
+		toolbarButtons[i]->setIconSize(pmap.rect().size());
+
+		//
+		
+	}
 }
--- pilotapp/Source/Widgets/Widgets/Toolbar/toolbar.h
+++ pilotapp/Source/Widgets/Widgets/Toolbar/toolbar.h
@@ -4,12 +4,27 @@
 #include <QWidget>
 
 #include <QPalette>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QString>
+#include <QPixmap>
+#include <QString>
+
+#define toolbarButtonSize 7
 
 class toolbarWidget : public QWidget{
 	Q_OBJECT
 
 private:
 	const float heightRatio = 0.14;
+
+	QHBoxLayout* hLayout;
+
+	// home, audio, display, battery/solar (electronics), environment, telemetry, graphs/stats
+	QPushButton* toolbarButtons[toolbarButtonSize]; 
+	const QString toolBarButtonIconNames[toolbarButtonSize] = {"home.png", "sound.png", "screen.png", "electronics.png", "environment.png", "telemetry.png", "graphs.png"};
+
+	void setupButtons();
 
 public:
 	toolbarWidget(QWidget* parent = nullptr);
