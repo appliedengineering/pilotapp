528144093004867390289890ddae21713f244bcb
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2022-05-03 23:04:06 -0700

    Set up power widget with labels

--- pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.cpp
+++ pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.cpp
@@ -2,6 +2,8 @@
 
 #include "../../../../../Backend/Utilities/utilities.h"
 
+#include <QPixmap>
+
 powerWidget::powerWidget(QWidget* parent){
     //this->widgetType = windowed;
     utilities::setPaletteColor(this, QPalette::Background, Qt::black);
@@ -13,7 +15,7 @@ powerWidget::powerWidget(QWidget* parent){
     this->setGeometry((parent->width()/2) - (powerWidgetWidth/2), parent->height() - powerWidgetHeight, powerWidgetWidth, powerWidgetHeight);
     utilities::setWidgetRoundedCorner(this, 10, {utilities::bottomLeft, utilities::bottomRight});
 
-    
+    renderContent();
 }
 
 powerWidget::~powerWidget(){
@@ -21,3 +23,133 @@ powerWidget::~powerWidget(){
 
 //
 
+
+
+void powerWidget::renderContent(){
+    mainContentHLayout = new QHBoxLayout(this);
+    
+    const int mainContentPadding = 10;
+
+    mainContentHLayout->setContentsMargins(mainContentPadding, mainContentPadding, mainContentPadding, mainContentPadding);
+    mainContentHLayout->setSpacing(0);
+
+    ///
+
+    powerSourceVLayout = new QVBoxLayout();
+
+    const int powerSourcePadding = 20;
+    const int powerSourceLayoutStretchFactor = 30;
+
+    powerSourceVLayout->setContentsMargins(powerSourcePadding, powerSourcePadding - mainContentPadding, powerSourcePadding, powerSourcePadding - mainContentPadding);
+    powerSourceVLayout->setSpacing(0);
+
+    mainContentHLayout->addLayout(powerSourceVLayout, powerSourceLayoutStretchFactor);
+
+    //
+
+    powerSourceIconLabel = new QLabel(this);
+
+    QPixmap iconPixMap(":/Assets/Icons/electronics.png");
+
+	iconPixMap.scaled(powerSourceIconLabel->width(), powerSourceIconLabel->width() * 0.8, Qt::KeepAspectRatio);
+
+	powerSourceIconLabel->setPixmap(iconPixMap);
+	powerSourceIconLabel->setScaledContents(1);
+
+    //utilities::setPaletteColor(powerSourceIconLabel, QPalette::Background, Qt::gray);
+
+    powerSourceVLayout->addWidget(powerSourceIconLabel);
+
+    ///
+
+    dataStackVLayout = new QVBoxLayout();
+
+    dataStackVLayout->setContentsMargins(0, 0, 0, 0);
+    dataStackVLayout->setSpacing(0);
+
+    mainContentHLayout->addLayout(dataStackVLayout, 100 - powerSourceLayoutStretchFactor);
+
+    //
+
+    dataValueHLayout = new QHBoxLayout();
+
+    dataValueHLayout->setContentsMargins(0, 0, 0, 0);
+    dataValueHLayout->setSpacing(0);
+
+    dataStackVLayout->addLayout(dataValueHLayout);
+
+    const int dataValueLabelSize = 20;
+
+    //
+
+    voltageValueLabel = new QLabel(this);
+
+    voltageValueLabel->setText("24");
+    utilities::setPaletteColor(voltageValueLabel, QPalette::Text, Qt::white, true);
+    voltageValueLabel->setAlignment(Qt::AlignHCenter);
+
+    QFont voltageValueLabelFont = voltageValueLabel->font();
+	voltageValueLabelFont.setPixelSize(dataValueLabelSize);
+	voltageValueLabel->setFont(voltageValueLabelFont);
+
+    dataValueHLayout->addWidget(voltageValueLabel);
+
+    //
+
+    currentValueLabel = new QLabel(this);
+
+    currentValueLabel->setText("20");
+    utilities::setPaletteColor(currentValueLabel, QPalette::Text, Qt::white, true);
+    currentValueLabel->setAlignment(Qt::AlignHCenter);
+
+    QFont currentValueLabelFont = currentValueLabel->font();
+    currentValueLabelFont.setPixelSize(dataValueLabelSize);
+    currentValueLabel->setFont(currentValueLabelFont);
+
+    dataValueHLayout->addWidget(currentValueLabel);
+
+    //
+
+    dataLabelHLayout = new QHBoxLayout();
+
+    dataLabelHLayout->setContentsMargins(0, 0, 0, 0);
+    dataLabelHLayout->setSpacing(0);
+
+    dataStackVLayout->addLayout(dataLabelHLayout);
+
+    const int dataLabelLabelSize = 10;
+
+    //
+
+    voltageLabelLabel = new QLabel(this);
+
+    voltageLabelLabel->setText("Volts");
+    utilities::setPaletteColor(voltageLabelLabel, QPalette::Text, Qt::white, true);
+    voltageLabelLabel->setAlignment(Qt::AlignHCenter);
+
+    QFont voltageLabelLabelFont = voltageLabelLabel->font();
+    voltageLabelLabelFont.setPixelSize(dataLabelLabelSize);
+    voltageLabelLabel->setFont(voltageLabelLabelFont);
+
+    dataLabelHLayout->addWidget(voltageLabelLabel);
+
+    //
+
+    currentLabelLabel = new QLabel(this);
+
+    currentLabelLabel->setText("Amps");
+    utilities::setPaletteColor(currentLabelLabel, QPalette::Text, Qt::white, true);
+    currentLabelLabel->setAlignment(Qt::AlignHCenter);
+
+    QFont currentLabelLabelFont = currentLabelLabel->font();
+    currentLabelLabelFont.setPixelSize(dataLabelLabelSize);
+    currentLabelLabel->setFont(currentLabelLabelFont);
+
+    dataLabelHLayout->addWidget(currentLabelLabel);
+
+    //
+}
+
+void powerWidget::resizeEvent(QResizeEvent*){
+    //mainContentHLayout->setStretchFactor(powerSourceVLayout, this->height() / this->width());
+}
--- pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.h
+++ pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.h
@@ -5,17 +5,34 @@
 #include "../../contentWidget.h"
 
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QResizeEvent>
 
 class powerWidget : public ContentWidget{
     Q_OBJECT
 
 private:
-    QHBoxLayout* hLayout = nullptr;
+    QHBoxLayout* mainContentHLayout;
 
+    QVBoxLayout* powerSourceVLayout;
+    QLabel* powerSourceIconLabel;
+
+    //
+
+    QVBoxLayout* dataStackVLayout;
     
+    QHBoxLayout* dataValueHLayout;
+    QLabel* voltageValueLabel;
+    QLabel* currentValueLabel;
+
+    QHBoxLayout* dataLabelHLayout;
+    QLabel* voltageLabelLabel;
+    QLabel* currentLabelLabel;
 
     void renderContent();
 
+    void resizeEvent(QResizeEvent*);
 public:
     powerWidget(QWidget* parent = nullptr);
     ~powerWidget();
