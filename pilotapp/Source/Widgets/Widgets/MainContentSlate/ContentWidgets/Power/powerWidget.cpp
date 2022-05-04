ea7638b23f67f00f2fe937055cabe11d5bdea322
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2022-05-03 19:50:49 -0700

    Add solar png + structure for powerWidget

--- pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.cpp
+++ pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Power/powerWidget.cpp
@@ -3,8 +3,15 @@
 #include "../../../../../Backend/Utilities/utilities.h"
 
 powerWidget::powerWidget(QWidget* parent){
-    this->widgetType = fullscreen;
+    //this->widgetType = windowed;
     utilities::setPaletteColor(this, QPalette::Background, Qt::blue);
+
+    //
+
+    //qInfo() << parent->size();
+    const int powerWidgetWidth = parent->width() * 0.6;
+    const int powerWidgetHeight = parent->height() * 0.15;
+    this->setGeometry((parent->width()/2) - (powerWidgetWidth/2), parent->height() - powerWidgetHeight, powerWidgetWidth, powerWidgetHeight);
 }
 
 powerWidget::~powerWidget(){
