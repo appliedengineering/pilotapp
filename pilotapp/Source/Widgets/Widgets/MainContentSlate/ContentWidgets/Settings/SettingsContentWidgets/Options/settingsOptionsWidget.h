da67d4ee0e98b86b871a832af64fed920d122e6a
Pyro-Impact <chaosdragonn1@gmail.com>
2022-08-01 23:18:51 -0700

    ui updates

--- pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Settings/SettingsContentWidgets/Options/settingsOptionsWidget.h
+++ pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Settings/SettingsContentWidgets/Options/settingsOptionsWidget.h
@@ -7,6 +7,7 @@
 #include <QScrollArea>
 #include <QScroller>
 #include <QLineEdit>
+#include <QLabel>
 
 class settingsOptionsWidget : public QWidget{
     Q_OBJECT
@@ -23,8 +24,44 @@ private:
 
     const int bottomActionsFontSize = 14;
     QHBoxLayout* bottomActionsLayout = nullptr;
+    QVBoxLayout* boatKernelLayout = nullptr;
+    QVBoxLayout* commsManagerLayout = nullptr;
+    QVBoxLayout* commsThreadLayout = nullptr;
+    QVBoxLayout* leftContentSlateLayout = nullptr;
+    QVBoxLayout* mapSettingsLayout = nullptr;
+    QVBoxLayout* mapLayout = nullptr;
     QPushButton* saveOptionsButton = nullptr;
     QPushButton* exitButton = nullptr;
+
+    QHBoxLayout* voltageBoxLayout = nullptr;
+    QLabel* boatKernelLabel = nullptr;
+    QLabel* minVoltageRangeLabel = nullptr;
+    QLabel* maxVoltageRangeLabel = nullptr;
+    QLabel* minMaxVoltageLabel = nullptr;
+
+    QLabel* communicationsLabel = nullptr;
+    QLabel* ipcSubAddressLabel = nullptr;
+    QLabel* ipcSubPortLabel = nullptr;
+    QLabel* scriptPairAddressLabel = nullptr;
+    QLabel* scriptPairPortLabel = nullptr;
+
+    QLabel* communicationsThreadLabel = nullptr;
+    QLabel* loopTimeoutLabel = nullptr;
+    QLabel* stopThreadLabel = nullptr;
+
+    QLabel* leftContentSlateLabel = nullptr;
+    QLabel* speedometerUnitsLabel = nullptr;
+
+    QHBoxLayout* maxCoordPrecisionLayout = nullptr;
+    QLabel* mapSettingsLabel = nullptr;
+    QLabel* maxCoordPrecisionLabel = nullptr;
+    QLabel* leftMaxCoordPrecisionLabel = nullptr;
+    QLabel* rightMaxCoordPrecisionLabel = nullptr;
+
+    QLabel* mapLabel = nullptr;
+    QLabel* buoyMarkerSizeLabel = nullptr;
+    QLabel* boatMarkerSizeLabel = nullptr;
+    QLabel* mapCacheDataLabel = nullptr;
 
     void setupLayout();
     void renderContent();
@@ -34,7 +71,12 @@ private:
     void renderDivider();
 
     void renderBottomActions();
-    
+    void renderBoatKernel();
+    void renderCommunicationsManager();
+    void renderCommunicationsThread();
+    void renderLeftContentSlate();
+    void renderMapSettings();
+    void renderMap();
     //
     
     void handleExit();
@@ -46,4 +88,3 @@ public:
 };
 
 #endif
-
