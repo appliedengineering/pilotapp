9e72fe336464fc60f767fd69c4349bcc2cc624b4
Pyro-Impact <chaosdragonn1@gmail.com>
2022-08-02 14:36:55 -0700

    UI Updates

--- pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Settings/SettingsContentWidgets/Options/settingsOptionsWidget.cpp
+++ pilotapp/Source/Widgets/Widgets/MainContentSlate/ContentWidgets/Settings/SettingsContentWidgets/Options/settingsOptionsWidget.cpp
@@ -6,6 +6,7 @@
 #include "../../../../../../../Backend/Utilities/utilities.h"
 #include "../../../../../../../Custom/TouchKeypad/touchKeypad.h"
 #include "../../../../../../../Custom/TouchInputWidgets/TouchNumericalLineEdit/touchNumericalLineEdit.h"
+#include "../../../../../../../Custom/RangeSlider/RangeSlider.h"
 
 //
 
@@ -14,10 +15,11 @@ settingsOptionsWidget::settingsOptionsWidget(QWidget* parent){
 
     setupLayout();
     renderContent();
-
 }
 
-settingsOptionsWidget::~settingsOptionsWidget(){
+settingsOptionsWidget::~settingsOptionsWidget()
+{
+    
 }
 
 //
@@ -60,14 +62,15 @@ void settingsOptionsWidget::setupLayout(){
 }
 
 void settingsOptionsWidget::renderContent(){
-    QLineEdit* l = new TouchNumericalLineEdit(this);
-
-    l->setText("0");
-
-    scrollAreaContentLayout->addWidget(l);
 
     //
 
+    renderBoatKernel();
+    renderCommunicationsManager();
+    renderCommunicationsThread();
+    renderLeftContentSlate();
+    renderMapSettings();
+    renderMap();
     renderDivider();
     renderBottomActions();
 }
@@ -125,14 +128,331 @@ void settingsOptionsWidget::renderBottomActions(){
     connect(exitButton, &QPushButton::released, this, &settingsOptionsWidget::handleExit);
 }
 
+void settingsOptionsWidget::renderBoatKernel()
+{
+    boatKernelLayout = new QVBoxLayout(this);
+    boatKernelLayout->setContentsMargins(0, 0, 0, 0);
+    //boatKernelLayout->setSpacing(horizontalPadding);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(boatKernelLayout);
+
+    boatKernelLabel = new QLabel(this);
+    boatKernelLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    boatKernelLabel->setText("Boat Kernel");
+    boatKernelLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    
+    leftLayout->addWidget(boatKernelLabel);
+
+    minMaxVoltageLabel = new QLabel(this);
+    minMaxVoltageLabel->setFrameStyle(QFrame::Plain);
+    minMaxVoltageLabel->setText("Min/Max Voltage");
+    minMaxVoltageLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    boatKernelLayout->addWidget(minMaxVoltageLabel);
+
+    voltageBoxLayout = new QHBoxLayout();
+    voltageBoxLayout->setContentsMargins(0, 0, 0, 0);
+    voltageBoxLayout->setSpacing(horizontalPadding);
+
+    boatKernelLayout->addLayout(voltageBoxLayout);
+
+    minVoltageRangeLabel = new QLabel(this);
+    //minVoltageRangeLabel->setAlignment(Qt::AlignHCenter);
+    minVoltageRangeLabel->setFixedWidth(30);
+    minVoltageRangeLabel->setText("0"); //need backend changes
+
+    voltageBoxLayout->addWidget(minVoltageRangeLabel);
+
+    RangeSlider* minMaxVoltageSlider = new RangeSlider(Qt::Horizontal, RangeSlider::DoubleHandles, this);
+    minMaxVoltageSlider->SetRange(0, 14);
+    minMaxVoltageSlider->SetUpperValue(14);
+    minMaxVoltageSlider->SetLowerValue(0);
+
+    voltageBoxLayout->addWidget(minMaxVoltageSlider);
+
+    maxVoltageRangeLabel = new QLabel(this);
+    //maxVoltageRangeLabel->setAlignment(Qt::AlignRight);
+    maxVoltageRangeLabel->setFixedWidth(30);
+    maxVoltageRangeLabel->setText("14"); //need backend changes
+
+    voltageBoxLayout->addWidget(maxVoltageRangeLabel);
+}
+
+void settingsOptionsWidget::renderCommunicationsManager()
+{
+    commsManagerLayout = new QVBoxLayout(this);
+    commsManagerLayout->setContentsMargins(0, 0, 0, 0);
+    commsManagerLayout->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(commsManagerLayout);
+
+    communicationsLabel = new QLabel(this);
+    communicationsLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    communicationsLabel->setText("Communications");
+    communicationsLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    leftLayout->addWidget(communicationsLabel);
+
+    ipcSubAddressLabel = new QLabel(this);
+    ipcSubAddressLabel->setFrameStyle(QFrame::Plain);
+    ipcSubAddressLabel->setText("IPC IP Address");
+    ipcSubAddressLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsManagerLayout->addWidget(ipcSubAddressLabel);
+
+    QLineEdit* ipcSubAddressNumericalInput = new TouchNumericalLineEdit(this);
+    ipcSubAddressNumericalInput->setAlignment(Qt::AlignRight);
+    ipcSubAddressNumericalInput->setFixedWidth(300);
+
+    commsManagerLayout->addWidget(ipcSubAddressNumericalInput);
+
+
+    ipcSubPortLabel = new QLabel(this);
+    ipcSubPortLabel->setFrameStyle(QFrame::Plain);
+    ipcSubPortLabel->setText("IPC Port");
+    ipcSubPortLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsManagerLayout->addWidget(ipcSubPortLabel);
+
+    QLineEdit* ipcSubPortNumericalInput = new TouchNumericalLineEdit(this);
+    ipcSubPortNumericalInput->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    ipcSubPortNumericalInput->setFixedWidth(300);
+    
+    commsManagerLayout->addWidget(ipcSubPortNumericalInput);
+
+    scriptPairAddressLabel = new QLabel(this);
+    scriptPairAddressLabel->setFrameStyle(QFrame::Plain);
+    scriptPairAddressLabel->setText("Script Pair Address");
+    scriptPairAddressLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsManagerLayout->addWidget(scriptPairAddressLabel);
+    
+    QLineEdit* scriptPairAddressNumericalInput = new TouchNumericalLineEdit(this);
+    scriptPairAddressNumericalInput->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    scriptPairAddressNumericalInput->setFixedWidth(300);
+    
+    commsManagerLayout->addWidget(scriptPairAddressNumericalInput);
+
+    scriptPairPortLabel = new QLabel(this);
+    scriptPairPortLabel->setFrameStyle(QFrame::Plain);
+    scriptPairPortLabel->setText("Script Pair Address");
+    scriptPairPortLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsManagerLayout->addWidget(scriptPairPortLabel);
+    
+    QLineEdit* scriptPairPortNumericalInput = new TouchNumericalLineEdit(this);
+    scriptPairPortNumericalInput->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    scriptPairPortNumericalInput->setFixedWidth(300);
+    
+    commsManagerLayout->addWidget(scriptPairPortNumericalInput);    
+}
+
+void settingsOptionsWidget::renderCommunicationsThread()
+{
+    commsThreadLayout = new QVBoxLayout(this);
+    commsThreadLayout->setContentsMargins(0, 0, 0, 0);
+    commsThreadLayout->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(commsThreadLayout);
+
+    communicationsThreadLabel = new QLabel(this);
+    communicationsThreadLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    communicationsThreadLabel->setText("Communications Thread");
+    communicationsThreadLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    leftLayout->addWidget(communicationsThreadLabel);
+
+    loopTimeoutLabel = new QLabel(this);
+    loopTimeoutLabel->setFrameStyle(QFrame::Plain);
+    loopTimeoutLabel->setText("Loop Timeout");
+    loopTimeoutLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsThreadLayout->addWidget(loopTimeoutLabel);
+
+    QLineEdit* loopTimeoutNumericalInput = new TouchNumericalLineEdit(this);
+    loopTimeoutNumericalInput->setAlignment(Qt::AlignRight);
+    loopTimeoutNumericalInput->setFixedWidth(300);
+
+    commsThreadLayout->addWidget(loopTimeoutNumericalInput);
+
+
+    stopThreadLabel = new QLabel(this);
+    stopThreadLabel->setFrameStyle(QFrame::Plain);
+    stopThreadLabel->setText("Stop Thread");
+    stopThreadLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    commsThreadLayout->addWidget(stopThreadLabel);
+
+    QLineEdit* stopThreadNumericalInput = new TouchNumericalLineEdit(this);
+    stopThreadNumericalInput->setAlignment(Qt::AlignRight);
+    stopThreadNumericalInput->setFixedWidth(300);
+
+    commsThreadLayout->addWidget(stopThreadNumericalInput);
+}
+
+void settingsOptionsWidget::renderLeftContentSlate()
+{
+    leftContentSlateLayout = new QVBoxLayout(this);
+    leftContentSlateLayout->setContentsMargins(0, 0, 0, 0);
+    leftContentSlateLayout->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(leftContentSlateLayout);
+
+    leftContentSlateLabel = new QLabel(this);
+    leftContentSlateLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    leftContentSlateLabel->setText("Left Content Slate");
+    leftContentSlateLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    leftLayout->addWidget(leftContentSlateLabel);
+
+    speedometerUnitsLabel = new QLabel(this);
+    speedometerUnitsLabel->setFrameStyle(QFrame::Plain);
+    speedometerUnitsLabel->setText("Speedometer Units");
+    speedometerUnitsLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    leftContentSlateLayout->addWidget(speedometerUnitsLabel);
+
+    QLineEdit* speedometerUnitsNumericalInput = new QLineEdit(this);
+    speedometerUnitsNumericalInput->setAlignment(Qt::AlignRight);
+    speedometerUnitsNumericalInput->setFixedWidth(300);
+
+    leftContentSlateLayout->addWidget(speedometerUnitsNumericalInput);
+}
+
+void settingsOptionsWidget::renderMapSettings()
+{
+    mapSettingsLayout = new QVBoxLayout(this);
+    mapSettingsLayout->setContentsMargins(0, 0, 0, 0);
+    //mapSettingsLayout->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(mapSettingsLayout);
+
+    mapSettingsLabel = new QLabel(this);
+    mapSettingsLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    mapSettingsLabel->setText("Map Settings");
+    mapSettingsLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    leftLayout->addWidget(mapSettingsLabel);
+
+    maxCoordPrecisionLabel = new QLabel(this);
+    maxCoordPrecisionLabel->setFrameStyle(QFrame::Plain);
+    maxCoordPrecisionLabel->setText("Max Coordinate Precision");
+    maxCoordPrecisionLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    mapSettingsLayout->addWidget(maxCoordPrecisionLabel);
+
+    maxCoordPrecisionLayout = new QHBoxLayout();
+    maxCoordPrecisionLayout->setContentsMargins(0, 0, 0, 0);
+    maxCoordPrecisionLayout->setSpacing(horizontalPadding);
+
+    mapSettingsLayout->addLayout(maxCoordPrecisionLayout);
+
+    leftMaxCoordPrecisionLabel = new QLabel(this);
+    leftMaxCoordPrecisionLabel->setFixedWidth(30);
+    leftMaxCoordPrecisionLabel->setText("0"); //need backend changes
+
+    maxCoordPrecisionLayout->addWidget(leftMaxCoordPrecisionLabel);
+
+    RangeSlider* maxCoordPrecisionSlider = new RangeSlider(Qt::Horizontal, RangeSlider::RightHandle, this);
+    maxCoordPrecisionSlider->SetRange(0, 100);
+    maxCoordPrecisionSlider->SetUpperValue(100);
+    maxCoordPrecisionSlider->SetLowerValue(0);
+
+    maxCoordPrecisionLayout->addWidget(maxCoordPrecisionSlider);
+
+    rightMaxCoordPrecisionLabel = new QLabel(this);
+    rightMaxCoordPrecisionLabel->setFixedWidth(30);
+    rightMaxCoordPrecisionLabel->setText("100"); //need backend changes
+
+    maxCoordPrecisionLayout->addWidget(rightMaxCoordPrecisionLabel);
+}
+
+void settingsOptionsWidget::renderMap()
+{
+    mapLayout = new QVBoxLayout(this);
+    mapLayout->setContentsMargins(0, 0, 0, 0);
+    mapLayout->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    auto leftLayout = new QVBoxLayout(this);
+    leftLayout->setContentsMargins(0, 0, 0, 0);
+    leftLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    scrollAreaContentLayout->addLayout(leftLayout);
+    scrollAreaContentLayout->addLayout(mapLayout);
+
+    mapLabel = new QLabel(this);
+    mapLabel->setStyleSheet("font-weight: bold; font-size: 20px");
+    mapLabel->setText("Map");
+    mapLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    leftLayout->addWidget(mapLabel);
+
+    buoyMarkerSizeLabel = new QLabel(this);
+    buoyMarkerSizeLabel->setFrameStyle(QFrame::Plain);
+    buoyMarkerSizeLabel->setText("Buoy Marker Size");
+    buoyMarkerSizeLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    mapLayout->addWidget(buoyMarkerSizeLabel);
+
+    QLineEdit* buoyMarkerSizeNumericalInput = new TouchNumericalLineEdit(this);
+    buoyMarkerSizeNumericalInput->setAlignment(Qt::AlignTop | Qt::AlignRight);
+    buoyMarkerSizeNumericalInput->setFixedWidth(300);
+
+    mapLayout->addWidget(buoyMarkerSizeNumericalInput);
+
+    boatMarkerSizeLabel = new QLabel(this);
+    boatMarkerSizeLabel->setFrameStyle(QFrame::Plain);
+    boatMarkerSizeLabel->setText("Boat Marker Size");
+    boatMarkerSizeLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    mapLayout->addWidget(boatMarkerSizeLabel);
+
+    QLineEdit* boatMarkerSizeNumericalInput = new TouchNumericalLineEdit(this);
+    boatMarkerSizeNumericalInput->setAlignment(Qt::AlignRight);
+    boatMarkerSizeNumericalInput->setFixedWidth(300);
+
+    mapLayout->addWidget(boatMarkerSizeNumericalInput);
+
+    mapCacheDataLabel = new QLabel(this);
+    mapCacheDataLabel->setFrameStyle(QFrame::Plain);
+    mapCacheDataLabel->setText("Map Cache Data");
+    mapCacheDataLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+
+    mapLayout->addWidget(mapCacheDataLabel);
+
+    QLineEdit* mapCacheDataNumericalInput = new QLineEdit(this);
+    mapCacheDataNumericalInput->setAlignment(Qt::AlignRight);
+    mapCacheDataNumericalInput->setFixedWidth(300);
+
+    mapLayout->addWidget(mapCacheDataNumericalInput);
+}
+
 //
 
 void settingsOptionsWidget::handleExit(){
     QMessageBox b;
-    b.setText("Close program");
+    b.setText("Exit.");
     b.setInformativeText("Are you sure?");
     b.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-    //b.setDefaultButton(QMessageBox::No);
+    b.setDefaultButton(QMessageBox::Yes);
 
     if (b.exec() == QMessageBox::Yes)
         utilities::findMainWindow()->closeApplication();
